d77354e77aba9a84d64eaa5bc7926304944b8fb3
Antonio Rudas <antoniojrudass@gmail.com>
2025-08-09 18:13:17 -0500

    feat:: backend config

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file: dotenv es un paquete que carga las variables de entorno desde un archivo .env a process.env
+
+const app = express(); // Create an Express application: Express es un framework de Node.js que facilita la creación de aplicaciones web
+const PORT = process.env.PORT;
+
+app.use(express.json()); // Parse JSON bodies: Express es un framework de Node.js que facilita la creación de aplicaciones web
+app.use(cors()); // Enable CORS: CORS es un mecanismo que permite que los recursos restringidos en una página web sean solicitados desde otro dominio fuera del dominio desde el cual se sirvió el primer recurso.
+app.use(helmet()); // Protect HTTP headers: Helmet es un middleware que ayuda a proteger la app
+app.use(morgan("dev")); // Log HTTP requests: Morgan es un middleware que ayuda a registrar las peticiones HTTP
+
+app.get("/test", (req, res) => {
+  console.log(res.getHeaders());
+  res.send("Hello from the test route");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
